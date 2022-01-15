ce3c037655b3d50b44f41c8fc270585eaffef725
Edison Fillus <efillus@tripadvisor.com>
2022-01-15 22:06:06 +0000

    Included Clock as a Bean in AssessmentApplication

--- assessment/src/main/java/com/abnamro/assessment/AssessmentApplication.java
+++ assessment/src/main/java/com/abnamro/assessment/AssessmentApplication.java
@@ -1,13 +1,22 @@
 package com.abnamro.assessment;
 
+import java.time.Clock;
+import java.time.ZoneOffset;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AssessmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssessmentApplication.class, args);
+	}
+
+	@Bean
+	public Clock clock(){
+		return Clock.system(ZoneOffset.UTC);
 	}
 
 }
