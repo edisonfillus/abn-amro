79dd099c39a337b62f7a22bc2c65ef7692028abd
Edison Fillus <efillus@tripadvisor.com>
2022-01-15 22:06:41 +0000

    Create configuration for Method Security Annotations

---
+++ assessment/src/main/java/com/abnamro/assessment/configs/security/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.abnamro.assessment.configs.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override // Authorization
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll()
+            .and().csrf().disable()
+            .exceptionHandling()
+            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+}
