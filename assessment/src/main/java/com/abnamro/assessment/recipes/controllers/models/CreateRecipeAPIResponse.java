3852633571845879212f9e6f50066581f8f319c3
Edison Fillus <efillus@tripadvisor.com>
2022-01-16 11:46:37 +0000

    Exposed RecipeRef in CreateRecipeAPIResponse

--- assessment/src/main/java/com/abnamro/assessment/recipes/controllers/models/CreateRecipeAPIResponse.java
+++ assessment/src/main/java/com/abnamro/assessment/recipes/controllers/models/CreateRecipeAPIResponse.java
@@ -2,12 +2,15 @@ package com.abnamro.assessment.recipes.controllers.models;
 
 import java.util.List;
 
+import com.abnamro.assessment.shared.references.RecipeRef;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
 
 @Value @Builder @Jacksonized
 public class CreateRecipeAPIResponse {
+
+    RecipeRef recipeRef;
 
     String name;
 
--- assessment/src/main/java/com/abnamro/assessment/recipes/services/dtos/RecipeDTO.java
+++ assessment/src/main/java/com/abnamro/assessment/recipes/services/dtos/RecipeDTO.java
@@ -3,11 +3,14 @@ package com.abnamro.assessment.recipes.services.dtos;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.abnamro.assessment.shared.references.RecipeRef;
 import lombok.Builder;
 import lombok.Value;
 
 @Value @Builder
 public class RecipeDTO {
+
+    RecipeRef recipeRef;
 
     String name;
 
