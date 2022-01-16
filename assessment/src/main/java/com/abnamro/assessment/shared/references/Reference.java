c5859e7cfb7418e0ff4ce166648e98ad4dbe781e
Edison Fillus <efillus@tripadvisor.com>
2022-01-16 00:45:40 +0000

    Included implementations of Comparator, Equals and HashCode in Reference

--- assessment/src/main/java/com/abnamro/assessment/shared/references/Reference.java
+++ assessment/src/main/java/com/abnamro/assessment/shared/references/Reference.java
@@ -1,6 +1,9 @@
 package com.abnamro.assessment.shared.references;
 
-public abstract class Reference {
+import java.util.Comparator;
+import java.util.Objects;
+
+public abstract class Reference implements Comparable<Reference> {
 
     public abstract String getValue();
 
@@ -9,4 +12,27 @@ public abstract class Reference {
         return getValue();
     }
 
+    @Override
+    public int compareTo(Reference other) {
+        return Comparator.comparing(obj -> obj.getClass().getName())
+                         .thenComparing(Object::toString)
+                         .compare(this, other);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Reference other = (Reference) obj;
+        return this.getValue().equals(other.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getValue());
+    }
 }
