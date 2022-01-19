7f21b815c85f0f3d0aa998d45cafa74811ca3269
Edison Fillus <efillus@tripadvisor.com>
2022-01-19 19:33:52 +0000

    Included an injection test on contextLoads to assert

--- assessment/src/test/java/com/abnamro/assessment/AssessmentApplicationTests.java
+++ assessment/src/test/java/com/abnamro/assessment/AssessmentApplicationTests.java
@@ -1,16 +1,28 @@
 package com.abnamro.assessment;
 
 import com.abnamro.assessment.shared.security.SecurityTestConfig;
+import com.abnamro.assessment.users.services.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Import(SecurityTestConfig.class)
 @SpringBootTest
 class AssessmentApplicationTests {
 
+	private final UserService userService;
+
+	@Autowired
+	public AssessmentApplicationTests(UserService userService) {
+		this.userService = userService;
+	}
+
 	@Test
 	void contextLoads() {
+		assertThat(userService).isNotNull();
 	}
 
 }
